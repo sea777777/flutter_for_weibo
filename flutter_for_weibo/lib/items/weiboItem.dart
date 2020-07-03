3ab16c13d4d3f58b7682db4879748b38d49bbd7c
sea777777 <lyc1234560720@126.com>
2020-07-03 09:48:50 +0800

    先提上再说

--- flutter_for_weibo/lib/items/weiboItem.dart
+++ flutter_for_weibo/lib/items/weiboItem.dart
@@ -1,60 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_for_weibo/models/WeiBoCard.dart';
 
-
 class WeiboItem extends StatelessWidget {
-
   final WeiBoCard data;
 
   const WeiboItem({Key key, this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(15),
       child: Expanded(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             Row(
               children: [
-                Image.network(
-                    '${data.user.profileImgUrl}',
-                    width: 60,
-                    height: 60,
-                    fit: BoxFit.cover,
-                ),
-                Text(
-                  '${data.user.screenName}',
-                    maxLines: 20,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF333333),
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: NetworkImage('${data.user.profileImgUrl}')),
                   ),
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        '${data.user.screenName}',
+                        maxLines: 20,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF333333),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        '${data.user.screenName}',
+                        maxLines: 20,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF333333),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
             Padding(padding: EdgeInsets.only(top: 30)),
             Row(
               children: <Widget>[
-                Text(
+                Expanded(
+                  child: Text(
                     '${data.rawText}',
                     maxLines: 20,
                     overflow: TextOverflow.clip,
                     style: TextStyle(
-                        fontSize: 13,
-                        color: Color(0xFF333333),
+                      fontSize: 13,
+                      color: Color(0xFF333333),
                     ),
+                  ),
                 ),
               ],
             ),
           ],
         ),
-      ),      
+      ),
     );
   }
 }
-
